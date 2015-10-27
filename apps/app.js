35fabfdba636f756282bac858b10331636577027
Sam Kozloff <samkozloff89@gmail.com>
2015-10-27 17:22:23 -0500

    truckign along

--- apps/app.js
+++ apps/app.js
@@ -1,17 +1,34 @@
+var query_number = 0;
+
 $(function() {
 	console.log("loaded");
-	$.getJSON("http://omdbapi.com/?s=Star%20Wars&r=json", function(data) {
-		var myData = data.Search;
-		showData(myData);
-		// for (var i = 0; i < myData.length; i++) {
-		// 	$("#search-results").append($('<span/>').html("Title: " + myData[i].Title + "<br>"));
-	});
+		$("#search-term").submit(function(event) {
+			event.preventDefault();
+			var searchTerm = $("#query").val();
+			$.getJSON("http://omdbapi.com/?s=" + searchTerm + "&r=json", function(data) {
+				var myData = data.Search;
+			try {
+				myData.length
+				showData(myData);
+				$("#query").val("");
+			 } catch(err) {
+				$("#failed-searches").append($("<p/>").text(searchTerm));
+				alert("No matches!");
 
+			};
+		});
+	});
 });
 
 function showData(data) {
-	$.each(data, function(index, obj) {
-		$("#search-results").append($("<span/>").html((index + 1) + " : " + obj.Title + "<br>"));
-	});
-	console.log(data);
+
+		data.length;
+		$("#search-results").prepend($('<hr>')).prepend($('<div/>').attr('id', query_number));
+		$.each(data, function(index, obj) {
+			console.log(query_number)
+			$('#' + query_number).append($("<span/>").html((index + 1) + " : " + obj.Title + "<br>"));
+		});
+		query_number++
+		console.log(data);
+
 };
